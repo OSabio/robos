a901c4320ff9181e116bae05485a118d87b60c23
OSabio <gahsabio@outlook.com>
2021-12-05 15:12:58 -0300

    ErrorBoundry

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,24 @@
+import React, { Component, render } from 'react';
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true} )
+    }
+
+    render() {
+
+        return this.state.hasError ?
+            <h1>Ops</h1>:
+            this.props.children
+    }
+}
+
+export default ErrorBoundry
